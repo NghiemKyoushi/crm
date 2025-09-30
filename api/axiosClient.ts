dcfd84d5fe8ca6d8f8bc328bc3802eabe35af68a
ducnghiem28082000 <ducnghiem28082000@gmail.com>
2025-09-30 09:53:43 +0700

    fix storage to cookies

--- api/axiosClient.ts
+++ api/axiosClient.ts
@@ -40,9 +40,13 @@ const processQueue = (error: any, token: string | null = null) => {
 };
 
 api.interceptors.request.use((config) => {
-  const accessToken = localStorage.getItem("accessToken");
-  if (accessToken) {
-    config.headers["Authorization"] = `Bearer ${accessToken}`;
+  // const accessToken = localStorage.getItem("accessToken");
+  // if (accessToken) {
+  //   config.headers["Authorization"] = `Bearer ${accessToken}`;
+  // }
+  const token = Cookies.get("accessToken");
+  if (token) {
+    config.headers["Authorization"] = `Bearer ${token}`;
   }
   return config;
 });
@@ -62,7 +66,9 @@ api.interceptors.response.use(
     const logout = () => {
       localStorage.clear();
       window.location.href = "/login";
-      Cookies.remove("token", { path: "" }); 
+      Cookies.remove("token", { path: "/" }); 
+      Cookies.remove("accessToken", { path: "" }); 
+      Cookies.remove("refreshToken", { path: "" }); 
     };
 
     if (err.response?.status === 401 && !originalRequest._retry) {
@@ -82,7 +88,8 @@ api.interceptors.response.use(
       originalRequest._retry = true;
       isRefreshing = true;
 
-      const refreshToken = localStorage.getItem("refreshToken");      
+      // const refreshToken = localStorage.getItem("refreshToken");
+      const refreshToken = Cookies.get("refreshToken");      
       if (!refreshToken) {
         logout();
         return Promise.reject(err);
@@ -106,7 +113,13 @@ api.interceptors.response.use(
           sameSite: "lax",
           secure: true,
         });
-        localStorage.setItem("accessToken", token);
+        // localStorage.setItem("accessToken", token);
+        Cookies.set("accessToken", token, {
+          expires: 1,
+          path: "/",
+          sameSite: "lax",
+          secure: true,
+        });
         processQueue(null, token);
 
         originalRequest.headers.Authorization = `Bearer ${token}`;
