16d879465ad961014e422e0f0a062f7386c22a20
ducnghiem28082000 <ducnghiem28082000@gmail.com>
2025-10-06 00:36:01 +0700

    update code fix fee setting

--- features/fee-settting/hooks/fee-setting.ts
+++ features/fee-settting/hooks/fee-setting.ts
@@ -47,7 +47,7 @@ export const useListGeneralPolicy = (customer_group_id?: number) => {
   return useQuery({
     queryKey: ["generalPolicy", customer_group_id],
     queryFn: () => getListGeneralPolicy({customer_group_id: customer_group_id}),
-    enabled: !!customer_group_id, // chỉ gọi khi có id
+    // enabled: !!customer_group_id, // chỉ gọi khi có id
   });
 };
 
